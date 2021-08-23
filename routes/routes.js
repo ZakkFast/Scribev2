3f41e2a8d2cb631dbe4469cb5bfd21c5d90ac84f
Zakk Fast <zakkfastpro@gmail.com>
2021-08-22 21:53:04 -0700

    fixed app name so itll actually work..

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 
-module.exports = savedDate => {
+module.exports = app => {
     fs.readFile('../db/db.json', 'utf8', (err, data) =>{
         if(err) throw err
         let notes = JSON.parse(data)
