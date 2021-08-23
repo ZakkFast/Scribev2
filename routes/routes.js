2ef073a790e8a7fd282f5c05882e85047dc13a47
Zakk Fast <zakkfastpro@gmail.com>
2021-08-22 21:45:22 -0700

    added outline comments to guide process

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,14 @@
 const path = require('path')
 const fs = require('fs')
 
+/*
+1. set up notes
+2. set up the get route for notes
+3. set up the post route for notes
+ 3.1. take new notes and put them into the database?
+4.set up get for notes, need to look into ids. maybe uuid?
+ 4.1. display the notes from the json by id?
+ 4.2. inject notes into html.
+ 4.3 possibly need function to update the JSON whenever a note is added (or deleted if I can figure it out..)
+5. looking into setting up a way to delete notes. look up express app documentation for delete method()
+*/
